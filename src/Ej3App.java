46a62726c4dcd56a96e5794453ec5142a02b8112
oandreazini <oriol_as_92@hotmail.com>
2022-07-08 17:26:15 +0200

    Fourth Commit

--- src/Ej3App.java
+++ src/Ej3App.java
@@ -19,6 +19,12 @@ public class Ej3App {
 		String entrada = JOptionPane.showInputDialog("Introduce un número:");
 		num = Integer.parseInt(entrada);
 		
+		/*
+		 * Se comprueva el número introducido es primo o no, con el bucle for buscamos otro
+		 * número que nos de como resto 0, si llegamos al número introducido sin encontrar 
+		 * ninguna división con resto 0 salimos del for y devolvemos un booleano true
+		 */
+		
 		for(int i=2; i<num; i++){
 			if(num%i==0)
 				return false;
---
+++ src/Ej4App.java
@@ -0,0 +1,27 @@
+import javax.swing.JOptionPane;
+
+public class Ej4App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		factorial(0);
+	}
+
+	public static void factorial (int num1) {
+		
+		int num2 = 1;
+		
+		String entrada = JOptionPane.showInputDialog("Introduce un número para calcular el factorial");
+		num1 = Integer.parseInt(entrada);
+		
+		/* Multiplicamos el número introducido por el num2, y con el bucle while vamos multiplicando
+		 * el resultado por un número menos
+		 * */
+		while(num1!=0) {
+			num2=num1*num2;
+			num1--;
+		}
+		JOptionPane.showMessageDialog(null,  num2);
+	}
+}

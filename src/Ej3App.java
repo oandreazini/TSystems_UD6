e2dc98660cad83d5f431a56169c63bf876a4ab16
oandreazini <oriol_as_92@hotmail.com>
2022-07-08 16:43:40 +0200

    Third Commit

--- src/Ej3App.java
+++ src/Ej3App.java
@@ -1,10 +1,28 @@
+import java.util.Arrays;
+
+import javax.swing.JOptionPane;
 
 public class Ej3App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		if(numPrimo(0) == true) {
+			JOptionPane.showMessageDialog(null,  " El número introducido es primo");
+		}else {
+			JOptionPane.showMessageDialog(null,  " El número introducido no es primo");
+		}
 	}
 	
-	//public static boolean ()
+	public static boolean numPrimo ( int num) {
+	
+		String entrada = JOptionPane.showInputDialog("Introduce un número:");
+		num = Integer.parseInt(entrada);
+		
+		for(int i=2; i<num; i++){
+			if(num%i==0)
+				return false;
+			}
+		return true;
+	}
 }
